ce0efff292f223dda04c9c7061e28b27097c1776
Luiz Gustavo <lg.lufiego@gmail.com>
2024-10-01 00:46:16 -0300

    v0.32 | Chakra UI changes. Files removed. Logo added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
 import Home from './pages/Home'
+import Footer from './components/footer'
+
 function App() {
   return (
+    <ChakraProvider>
     <div style={{ backgroundImage: `url('/images/background-site.jpg')` }} className="App">
       <Home></Home>
+      <Footer></Footer>
     </div>
+    </ChakraProvider>
+    
   );
 }
 
